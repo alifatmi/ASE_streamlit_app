01fdce18ff639bda7112bb823282cde9aaab3226
talhazansari <talha.zahid.ansari@gmail.com>
2024-11-28 23:17:28 +0100

    make codes changes

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ from config import pdict
 @ht.timer
 def main():
     """Main: Generation of Streamlit App for visualizing electric charging stations & residents in Berlin"""
+    # Load geographic data for postal codes
+    df_postal_codes = pd.read_csv("datasets/" + pdict["file_geodat_plz"], delimiter=";")
 
-    df_geodat_plz = pd.read_csv("datasets/" + pdict["file_geodat_plz"], delimiter=";")
+    # Load and preprocess location station data
+    df_location_stations = pd.read_csv("datasets/" + pdict["file_lstations"], delimiter=",")
+    df_preprocessed_stations = m1.preprop_lstat(df_location_stations, df_postal_codes, pdict)
+    gdf_station_occurrences_by_plz = m1.count_plz_occurrences(df_preprocessed_stations)
 
-    df_lstat = pd.read_csv("datasets/" + pdict["file_lstations"], delimiter=",")
-    df_lstat2 = m1.preprop_lstat(df_lstat, df_geodat_plz, pdict)
-    gdf_lstat3 = m1.count_plz_occurrences(df_lstat2)
+    # Load and preprocess resident data
+    df_resident_data = pd.read_csv("datasets/" + pdict["file_residents"], delimiter=",")
+    gdf_preprocessed_residents = m1.preprop_resid(df_resident_data, df_postal_codes, pdict)
 
-    df_residents = pd.read_csv("datasets/" + pdict["file_residents"], delimiter=",")
-    gdf_residents2 = m1.preprop_resid(df_residents, df_geodat_plz, pdict)
-
-    m1.make_streamlit_electric_Charging_resid(gdf_lstat3, gdf_residents2)
+    # Generate Streamlit visualization for electric charging and residents
+    m1.make_streamlit_electric_Charging_resid(gdf_station_occurrences_by_plz, gdf_preprocessed_residents)
 
 
 if __name__ == "__main__":
